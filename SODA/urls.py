c0dfcfddb3c09fe707bf5b87081f4b4d3d3c3208
mashaoxu <244515206@qq.com>
2021-11-15 13:49:45 +0800

    摄像头模块

--- SODA/urls.py
+++ SODA/urls.py
@@ -7,8 +7,9 @@ from SODA.views import *
 urlpatterns = [
     path('login', do_login),
     path('register', do_register),
-    # path('camera/list', camera_list),
+    path('api/get_camera_list', camera_list),
     path('api/distribution', distribution),
-    path('api/get_predict_list',get_predict_list)
+    path('api/get_predict_list',get_predict_list),
+    path('api/get_map', get_map),
     # path('forecast/list',  passenger_flow_forecast_list),
     ]
